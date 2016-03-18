c58dc15a0d9372acc77ef0e3390fb797ca2d3c23
lpetrescu <laurp@aurelon.ro>
2016-03-18 11:06:34 +0200

    Added tray icon

--- Source/LunchApp/View/MainWindow.h
+++ Source/LunchApp/View/MainWindow.h
@@ -5,6 +5,8 @@
 #include "ui_MainWindow.h"
 #include "MetroView.h"
 
+class QSystemTrayIcon;
+
 class MainWindow : public QWidget
 {
 	Q_OBJECT
@@ -13,9 +15,14 @@ public:
 				MainWindow( QWidget *parent = 0 );
 
 				~MainWindow();
-private:
-	Ui::MainWindow	ui;
-	MetroView*		metroView;
+	
+	void		showTrayMessage( const QString& msg );
 
-	void		SendWeek();
+private:
+	Ui::MainWindow		ui;
+	MetroView*			metroView;
+	QSystemTrayIcon*	trayIcon;
+
+	void				SendWeek();
+	void				setupTray();
 };
