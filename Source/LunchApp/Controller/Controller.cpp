268fcf4e428cd12672f8b712d0b1386088f391b1
lpetrescu <laurp@aurelon.ro>
2016-03-23 10:11:03 +0200

    Before GUI merge

--- Source/LunchApp/Controller/Controller.cpp
+++ Source/LunchApp/Controller/Controller.cpp
@@ -45,44 +45,6 @@ void Controller::selectionChangedOn( const Dish& dish )
 void Controller::requestWeek( QDateTime startDate, QDateTime endDate )
 {
 	dataTransfer->getMenu(startDate, endDate);
-	// Make request to database
-	// TO DO
-	/*
-	// Dummy data
-	Sleep( 1000 );
-
-	std::vector<Dish> dishesVect;
-	dishesVect.push_back( Dish( "Quesadilla cu pui",
-		"tortilla  piept de pui  cascaval  ardei gras  ceapa  patrunjel  ulei  boia  usturoi  oregano  sare",
-		QPixmap("Resources/supa2.png"), 1 ) );
-	dishesVect.push_back( Dish( "Aripioare de pui cu crusta de porumb",
-		"tortilla  piept de pui  cascaval  ardei gras  ceapa  patrunjel  ulei  boia  usturoi  oregano  sare",
-		QPixmap("Resources/supa3.png"), 1 ) );
-	dishesVect.push_back( Dish( "Pastrav pane cu spanac",
-		"tortilla  piept de pui  cascaval  ardei gras  ceapa  patrunjel  ulei  boia  usturoi  oregano  sare",
-		QPixmap("Resources/salata2.png"), 2 ) );
-
-	std::vector<Day> daysVect;
-
-	std::random_shuffle( dishesVect.begin(), dishesVect.end() );
-	daysVect.push_back( Day( "Luni", dishesVect ) );
-
-	std::random_shuffle( dishesVect.begin(), dishesVect.end() );
-	daysVect.push_back( Day( "Marti", dishesVect ) );
-
-	std::random_shuffle( dishesVect.begin(), dishesVect.end() );
-	daysVect.push_back( Day( "Miercuri", dishesVect ) );
-
-	std::random_shuffle( dishesVect.begin(), dishesVect.end() );
-	daysVect.push_back( Day( "Joi", dishesVect ) );
-
-	std::random_shuffle( dishesVect.begin(), dishesVect.end() );
-	daysVect.push_back( Day( "Vineri", dishesVect ) );
-
-	Week week( "1 - 6 Martie", daysVect );
-	*/
-	
-	//emit weekArrived( week );
 }
 
 void Controller::dataFinished(const Week& week)
