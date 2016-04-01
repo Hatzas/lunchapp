dd149e5290ff1c3e032d4cddea5d7797f03506ba
lpetrescu <laurp@aurelon.ro>
2016-04-01 10:02:23 +0300

    Small change

--- Source/LunchApp/MainWindow.cpp
+++ Source/LunchApp/MainWindow.cpp
@@ -35,8 +35,6 @@ void MainWindow::showTrayMessage( const QString& msg )
 {
 	NotificationWindow* customWindow = new NotificationWindow( msg );
 	customWindow->show();
-
-	//trayIcon->showMessage(tr("Lunch App"), msg, QSystemTrayIcon::Information);
 }
 
 void MainWindow::onTrayActivation(QSystemTrayIcon::ActivationReason reason)
